7800f2f5c60f3de5b0565be225fd23339e206370
github_DASorescu <srsdln97@gmail.com>
2022-07-21 18:43:24 +0200

    first-commit

---
+++ src/components/Title.vue
@@ -0,0 +1,26 @@
+
+<template>
+<main>
+    <h1>La mia prima App con Vue CLI</h1>
+</main>
+</template>
+
+<script>
+export default{
+    name:"Title",
+}
+</script>
+
+<style scoop>
+h1{
+    color: red;
+}
+</style>
+
+
+
+
+
+
+
+
